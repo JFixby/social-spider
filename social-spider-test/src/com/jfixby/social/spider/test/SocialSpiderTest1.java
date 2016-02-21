0255f9d5402d3e84679d29ba6b21171a9f4abccb
J Fixby <github@jfixby.com>
2016-02-21 21:52:31 +0100

    ...

---
+++ social-spider-test/src/com/jfixby/social/spider/test/SocialSpiderTest1.java
@@ -0,0 +1,12 @@
+package com.jfixby.social.spider.test;
+
+import com.jfixby.red.desktop.DesktopAssembler;
+
+public class SocialSpiderTest1 {
+
+	public static void main(String[] args) {
+		DesktopAssembler.setup();
+
+	}
+
+}
